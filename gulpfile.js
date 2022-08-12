909fb61d6eed104f7f02a1ab254fd5ab9206de66
Javier Blanco <javierblancoit@gmail.com>
2022-08-12 21:48:19 +0100

    make our code compatible with different browsers with postcss and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+
 
 function css( done ) {
     // Compilar SASS
     // Pasos: 1. Localizar archivos SASS, 2. Compilar archivos SASS, 3. Guardar archivos SCSS
 
-    src('src/scss/app.scss')
-        .pipe(sass())
-        .pipe(dest('build/css'));
+    src('src/scss/app.scss') // identifica el archivo a compilar
+        .pipe(sass()) // compila el archivo resultado del paso anterior
+        // .pipe(sass({ outputStyle: 'compressed' }))
+        // .pipe(sass({ outputStyle: 'expanded' }))
+        .pipe( postcss( [ autoprefixer() ] )) // create compatible version for different browser, whose list has to be specified in the package.json
+        .pipe(dest('build/css')); // crea una nueva carpeta en la que ubica el archivo resltante del paso anterior
 
     done();
 }
